20d58cedea5daf1a9aca4a9b8545e465c86619d5
Astronomia Observacional <aobs@wasabi.das.uchile.cl>
2016-10-24 19:15:38 -0300

    Added final code scripts.

---
+++ code/aperture.py
@@ -0,0 +1,142 @@
+from astropy.io import fits
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import scipy as sp
+import numpy as np
+from zscale import zscale
+
+'''
+Codigo para HDU1
+'''
+
+master_bias = fits.getdata("../master/master_bias.fits", 1)
+master_flats = fits.getdata("../master/master_flats.fits", 1)
+
+sci1 = fits.getdata("../data/dat.025.fits", 1)  # Get HDU1
+
+sci11 = sp.divide(sci1 - master_bias, master_flats)
+
+mn, mx = zscale(sci11)
+
+plt.clf()
+plt.imshow(sci11, vmin=mn, vmax=mx)
+plt.show()
+
+x = 411  # 232
+y = 1838  # 671
+SR = 48  # 30
+stamp = sci11[y - SR:y + SR, x - SR:x + SR]
+
+
+def centroid(stamp):
+    """
+    Calcula el centro de la estrella viendo un centro de masasx
+    con el flujo.
+
+    Parameters
+    ----------
+    stamp : (N,)array_like
+            Arreglo en 2-D, representa una seccion de imagen que
+            engloba a una estrella.
+    Returns
+    -------
+    cx : float
+         Coordenada x del centro de la estrella.
+
+    cy : float
+         Coordenada y del centro de la estrella.
+    """
+    # Se crean vectores con los indices x e y de la estampilla.
+    x_vect = sp.arange(0, sp.shape(stamp)[1])
+    y_vect = sp.arange(0, sp.shape(stamp)[0])
+    # Se estima un centro de la estrella.
+    cx = sp.median(x_vect)
+    cy = sp.median(y_vect)
+    # Se calcula la coordenada x del centro de la estrella.
+    sum_x = sp.nansum(x_vect * stamp[cy, :])
+    cx = sum_x / sp.nansum(stamp[cy, :])
+    # Se calcula la coordenada y del centro de la estrella.
+    sum_y = sp.nansum(y_vect * stamp[:, cx])
+    cy = sum_y / sp.nansum(stamp[:, cx])
+    return cx, cy
+
+
+def radial_profile(stamp, cy, cx):
+    """
+    Grafica el perfil radial de una estrella englobada dentro de stamp
+    con centro en cx,cy
+
+    Parameters
+    ----------
+    stamp : (N,)array_like
+            Arreglo en 2-D, representa una seccion de imagen que
+            engloba a una estrella.
+
+    cx : float
+         Coordenada x del centro de la estrella.
+
+    cy : float
+         Coordenada y del centro de la estrella.
+    """
+    x_len = sp.shape(stamp)[1]
+    vect_x = sp.arange(0, x_len - int(cx))
+    flux_x = stamp[int(cx):x_len, cy]
+    plt.clf()
+    plt.plot(vect_x, flux_x)
+    plt.show()
+
+
+def distance(y1, x1, y2, x2):
+    return sp.sqrt((y2 - y1)**2 + (x2 - x1)**2)
+
+
+def ap_phot(stamp, cy, cx, ap, sky1, sky2):
+    """
+    Calcula la fotometria de apertura de una estrella dentro de
+    una porcion stamp de una observacion; caracterizada por un centro de
+    masas cy, cx; con un radio ap; y porcion sky2 - sky1 de cielo.
+
+    Parameters
+    ----------
+    stamp : (N,)array_like
+            Arreglo en 2-D, representa una seccion de imagen que
+            engloba a una estrella.
+
+    cx : float
+         Coordenada x del centro de la estrella.
+
+    cy : float
+         Coordenada y del centro de la estrella.
+
+    ap : float
+         Radio estimado de la estrella.
+
+    sky1 : float
+         Radio menor del anillo que representa el cielo en la imagen de la
+         estrella.
+
+    sky2 : float
+         Radio mayor del anillo que representa el cielo en la imagen de la
+         estrella.
+    """
+    x_index = sp.arange(sp.shape(stamp)[0])
+    y_index = sp.arange(sp.shape(stamp)[1])
+    dist = distance(y_index[cy], x_index[cx], y_index, x_index)
+
+    star = sp.where(dist <= ap)[0]
+    flux = sum(stamp[star, star])
+
+    sky = sp.where((dist > sky1) * (dist < sky2))[0]
+    sky_flux = sum(stamp[sky, sky])
+
+    flux_f = flux - sky_flux
+
+    return flux_f
+
+cx, cy = centroid(stamp)
+vmin, vmax = zscale(stamp)
+plt.imshow(stamp, vmin=vmin, vmax=vmax)
+plt.show()
+radial_profile(stamp, cx, cy)
+flx = ap_phot(stamp, cy, cx, 10, 25, 30)
+print(flx)
---
+++ code/master.py
@@ -0,0 +1,161 @@
+"""
+Obtencion de Master Bias y Master Flats
+"""
+
+from astropy.io import fits
+import scipy as sp
+import matplotlib.pyplot as plt
+
+path = "../data/dat."
+"""
+Esto ya lo sabemos
+bias_counter = flats_counter = 0
+k = 1
+skipped_nums=[]
+
+while 1:
+    file = "%03d.fits" % k
+    file_path = path + file
+    try:
+        img = fits.open(file_path)
+    except IOError:
+        skipped_nums.append(k)
+        k += 1
+        continue
+    header = img[0].header['object']
+    if header == 'bias':
+        bias_counter += 1
+    elif header == 'dome flat I Bessell':
+        flats_counter += 1
+    else:
+        break
+    k += 1
+    img.close()
+
+print("Hay {} bias y {} flats (dome flat I Bessell).".format(bias_counter,
+flats_counter))
+"""
+
+biases1 = sp.array([fits.getdata("../data/dat.%03d.fits" % n, 1)
+                    for n in range(1, 12)])
+biases2 = sp.array([fits.getdata("../data/dat.%03d.fits" % n, 2)
+                    for n in range(1, 12)])
+biases3 = sp.array([fits.getdata("../data/dat.%03d.fits" % n, 3)
+                    for n in range(1, 12)])
+biases4 = sp.array([fits.getdata("../data/dat.%03d.fits" % n, 4)
+                    for n in range(1, 12)])
+
+header_bias0 = fits.getheader("../data/dat.001.fits", 0)
+header_bias1 = fits.getheader("../data/dat.001.fits", 1)
+header_bias2 = fits.getheader("../data/dat.001.fits", 2)
+header_bias3 = fits.getheader("../data/dat.001.fits", 3)
+header_bias4 = fits.getheader("../data/dat.001.fits", 4)
+
+master_bias_mean1 = sp.mean(biases1, axis=0)
+master_bias_mean2 = sp.mean(biases2, axis=0)
+master_bias_mean3 = sp.mean(biases3, axis=0)
+master_bias_mean4 = sp.mean(biases4, axis=0)
+
+primary = fits.PrimaryHDU(header=header_bias0)
+bias1 = fits.ImageHDU(data=master_bias_mean1, header=header_bias1)
+bias2 = fits.ImageHDU(data=master_bias_mean2, header=header_bias2)
+bias3 = fits.ImageHDU(data=master_bias_mean3, header=header_bias3)
+bias4 = fits.ImageHDU(data=master_bias_mean4, header=header_bias4)
+l = [primary, bias1, bias2, bias3, bias4]
+hduList = fits.HDUList(l)
+hduList.writeto("../master/master_bias.fits")
+
+"""fits.setval("../master/master_bias.fits", 'BZERO',
+            value=header_bias1['BZERO'], ext=1)
+fits.setval("../master/master_bias.fits", 'BSCALE',
+            value=header_bias1['BSCALE'], ext=1)
+fits.setval("../master/master_bias.fits", 'BZERO',
+            value=header_bias2['BZERO'], ext=2)
+fits.setval("../master/master_bias.fits", 'BSCALE',
+            value=header_bias2['BSCALE'], ext=2)
+fits.setval("../master/master_bias.fits", 'BZERO',
+            value=header_bias3['BZERO'], ext=3)
+fits.setval("../master/master_bias.fits", 'BSCALE',
+            value=header_bias3['BSCALE'], ext=3)
+fits.setval("../master/master_bias.fits", 'BZERO',
+            value=header_bias4['BZERO'], ext=4)
+fits.setval("../master/master_bias.fits", 'BSCALE',
+            value=header_bias4['BSCALE'], ext=4)"""
+
+fits.info("../master/master_bias.fits")
+
+###################################################
+
+flats1 = sp.array([fits.getdata("../data/dat.%03d.fits" % n, 1)
+                   for n in range(12, 13)])
+flats2 = sp.array([fits.getdata("../data/dat.%03d.fits" % n, 2)
+                   for n in range(12, 13)])
+flats3 = sp.array([fits.getdata("../data/dat.%03d.fits" % n, 3)
+                   for n in range(12, 13)])
+flats4 = sp.array([fits.getdata("../data/dat.%03d.fits" % n, 4)
+                   for n in range(12, 13)])
+
+header_flats0 = fits.getheader("../data/dat.012.fits", 0)
+header_flats1 = fits.getheader("../data/dat.012.fits", 1)
+header_flats2 = fits.getheader("../data/dat.012.fits", 2)
+header_flats3 = fits.getheader("../data/dat.012.fits", 3)
+header_flats4 = fits.getheader("../data/dat.012.fits", 4)
+
+# Primary Flats
+primary = fits.PrimaryHDU(header=header_flats0)
+
+# imageHDU1
+raw_flat = sp.array([fits.getdata("../data/dat.%03d.fits" % n, 1)
+                     for n in range(12, 23)])
+
+flats = raw_flat - master_bias_mean1
+
+mean_flats = sp.mean(flats)
+
+flats_normalizado = sp.divide(flats, mean_flats)
+master_flats_norm = sp.mean(flats_normalizado, axis=0)
+
+flats_img1 = fits.ImageHDU(data=master_flats_norm, header=header_flats1)
+
+# imageHDU2
+raw_flat = sp.array([fits.getdata("../data/dat.%03d.fits" % n, 2)
+                     for n in range(12, 23)])
+
+flats = raw_flat - master_bias_mean2
+
+mean_flats = sp.mean(flats)
+
+flats_normalizado = sp.divide(flats, mean_flats)
+master_flats_norm = sp.mean(flats_normalizado, axis=0)
+
+flats_img2 = fits.ImageHDU(data=master_flats_norm, header=header_flats2)
+
+# imageHDU3
+raw_flat = sp.array([fits.getdata("../data/dat.%03d.fits" % n, 3)
+                     for n in range(12, 23)])
+
+flats = raw_flat - master_bias_mean3
+
+mean_flats = sp.median(flats)
+
+flats_normalizado = sp.divide(flats, mean_flats)
+master_flats_norm = sp.mean(flats_normalizado, axis=0)
+
+flats_img3 = fits.ImageHDU(data=master_flats_norm, header=header_flats3)
+
+# imageHDU4
+raw_flat = sp.array([fits.getdata("../data/dat.%03d.fits" % n, 4)
+                     for n in range(12, 23)])
+
+flats = raw_flat - master_bias_mean4
+
+mean_flats = sp.median(flats)
+
+flats_normalizado = sp.divide(flats, mean_flats)
+master_flats_norm = sp.mean(flats_normalizado, axis=0)
+
+flats_img4 = fits.ImageHDU(data=master_flats_norm, header=header_flats4)
+l = [primary, flats_img1, flats_img2, flats_img3, flats_img4]
+hduList = fits.HDUList(l)
+hduList.writeto("../master/master_flats.fits")
+fits.info("../master/master_flats.fits")
